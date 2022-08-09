59293e2ccf480d9315e5fdb54e4c57bdea3d473d
Fer <sombondrolo@gmail.com>
2022-08-09 18:50:04 -0300

    Created users_controller_spec and fix contacts_controller_spec

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -285,27 +285,3 @@ describe ContactsController do
     end
   end
 end
-
-  # estas no andan o no se donde van
-    # describe 'GET #index' do
-    #   it "collects users into @users" do
-    #     user = create(:user)
-    #     get :index
-    #     expect(assigns(:users)).to match_array [@user,user]
-    #   end
-    
-    #   it "renders the :index template" do
-    #     get :index
-    #     expect(response).to render_template :index
-    #   end
-    # end
-    
-    # it "GET #new denies access" do
-    #   get :new
-    #   expect(response).to redirect_to root_url
-    # end
-    
-    # it "POST#create denies access" do
-    #   post :create, user: attributes_for(:user)
-    #   expect(response).to redirect_to root_url
-    # end
