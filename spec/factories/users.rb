9e0ec702536ccf5d47fa3ca6a7809f4e08fa4b4a
Fer <sombondrolo@gmail.com>
2022-08-11 15:51:06 -0300

    users factory as it was

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    # email { Faker::Internet.email }
-    email 'admin@test.com'
+    email { Faker::Internet.email }
+    # email 'admin@test.com'
     password 'secret'
     password_confirmation 'secret'
     
