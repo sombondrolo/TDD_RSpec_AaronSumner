692d6d2ea0bf47f1588bf750708b1257e862cd6f
Fer <sombondrolo@gmail.com>
2022-08-12 16:11:36 -0300

    Defined news_release factory, added require login news_releases controller spec, added before_action authenticate in news_releases_controller

--- spec/controllers/news_releases_controller_spec.rb
+++ spec/controllers/news_releases_controller_spec.rb
@@ -18,6 +18,24 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
+describe NewsReleasesController, focus: true do
+  describe 'GET #new' do
+    it "requires login" do
+      get :new
+      expect(response).to require_login
+    end
+  end
+
+  describe "POST #create" do
+    it "requires login" do
+      post :create, news_release: attributes_for(:news_release)
+      expect(response).to require_login
+    end
+  end
+end
+
+
+=begin            # specs generadas por el scaffold
 RSpec.describe NewsReleasesController, :type => :controller do
 
   # This should return the minimal set of attributes required to create a valid
@@ -157,3 +175,4 @@ RSpec.describe NewsReleasesController, :type => :controller do
   end
 
 end
+=end
--- spec/factories/news_releases.rb
+++ spec/factories/news_releases.rb
@@ -1,9 +1,19 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
   factory :news_release do
-    title "MyString"
-    released_on "2022-08-11"
-    body "MyText"
+    title "Test news release"
+    released_on 1.day.ago
+    body { Faker::Lorem.paragraph }
   end
 end
+
+
+# FactoryGirl.define do
+#   factory :news_release do
+#     title "MyString"
+#     released_on "2022-08-11"
+#     body "MyText"
+#   end
+# end
