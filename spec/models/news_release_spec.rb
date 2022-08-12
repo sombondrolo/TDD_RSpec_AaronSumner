beb4d4a0121a4ef4e2855e82487862c87879f9b8
Fer <sombondrolo@gmail.com>
2022-08-12 14:48:53 -0300

    strftime error

--- spec/models/news_release_spec.rb
+++ spec/models/news_release_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe NewsRelease, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe NewsRelease, :type => :model do        # sintaxis autoscaffoldeada
+describe NewsRelease, type: :model, focus: true do
+  it "returns the formatted date and title as a string" do
+    news_release = NewsRelease.new(
+      released_on: '2013-07-31',
+      title: 'BigCo hires new CEO')
+    expect(news_release.title_with_date).to \
+      eq '2013-07-31: BigCo hires new CEO'
+  end
 end
